ab0989c99361d50528ff7b5006056666bd84bde5
Paramveer Singh <paramveer.singh238@outlook.com>
2025-02-25 21:18:20 -0800

    Add code to pivot dataframe for easier calculation

--- src/passenger_count.py
+++ src/passenger_count.py
@@ -26,3 +26,13 @@ def passenger_count(start_date, end_date, control_point: str = 'all') -> dict:
     # Load data and parse dates
     df = pd.read_csv('../data/raw/data.csv').drop(columns='Unnamed: 0')
     df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y')
+
+    # Pivot dataframe to calculate difference for all passenger types
+    diff_df = df.pivot(index=['date', 'control_point', 'passenger_origin'],
+                       columns=['travel_type'],
+                       values='passenger_count'
+                       ).reset_index(
+
+    ).drop(
+        columns='passenger_origin'
+    ).groupby(['date', 'control_point']).agg('sum').reset_index()
