3b5e0d2953a5202199647ab1e7f09c6132be3581
hovhannisyannelli <hovhannisyannelli2@gmail.com>
2025-02-25 20:06:35 -0800

    updated requirements.txt and app.py layout

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from dash import Dash, html, dcc  # type: ignore
 import dash_bootstrap_components as dbc  # type: ignore
 import pandas as pd
 from callbacks import register_callbacks  # Import the callback registration function
+from datetime import timedelta
 
 # Load data to get control point values
 DATA_PATH = "data/raw/data.csv"
@@ -26,7 +27,7 @@ sidebar = html.Div(
         html.H2("Filters", className="display-6"),
         html.Hr(),
         html.P("Select time period:"),
-        dcc.DatePickerRange(id="date_picker", end_date=last_date),
+        dcc.DatePickerRange(id="date_picker", start_date=last_date - timedelta(days=5), end_date=last_date, max_date_allowed=last_date ), 
         html.Br(),
         html.P("Select control point:"),
         dcc.Dropdown(id="control_point_dropdown", options=control_point_options, multi=True),
@@ -75,9 +76,8 @@ content = html.Div(
         html.Hr(),
         html.H3("Volume of Control Point Traffic"),
         dbc.Row([
-            dbc.Col(dcc.Graph(id="control_point_traffic"), width=6),  # Added traffic graph
-            dbc.Col(html.Div(id="map"), width=6),  # Map for control points
-        ]),
+            dbc.Col(html.Div(id="map", style={"width": "100%", "height": "600px"}), width=12),  # Full-width map
+]),
     ],
     style={"marginLeft": "22%", "padding": "20px"},
 )
