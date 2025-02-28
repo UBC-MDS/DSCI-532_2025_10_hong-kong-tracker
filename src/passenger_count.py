a010be93091f573c458b29637a91b97e03950428
Paramveer Singh <paramveer.singh238@outlook.com>
2025-02-27 21:43:02 -0800

    Update function definition to take in loaded dataframe

--- src/passenger_count.py
+++ src/passenger_count.py
@@ -5,18 +5,20 @@
 import pandas as pd
 import altair as alt
 
-def passenger_count(start_date, end_date, control_point: list[str] = 'all') -> dict:
+def passenger_count(df, start_date, end_date, control_point: list[str] = None) -> dict:
     """
     Function used with callback to return passenger count chart
 
     Parameters
     ----------
+    df : pd.DataFrame
+        Loaded Hong Kong traffic dataframe
     start_date : Date
         Start date of the data to look at
     end_date : Date
         End date of the data to look at
     control_point : list[str], optional
-        Control point to filter for. Defaults to 'all' for all control points
+        Control point to filter for. Defaults to None for all control points
 
     Returns
     -------
