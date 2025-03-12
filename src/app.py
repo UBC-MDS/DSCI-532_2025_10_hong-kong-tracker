82d5037f418d9f92b977c53e8bf7b2e7eef9afcd
Hankun Xiao <xh1319@student.ubc.ca>
2025-03-11 22:50:32 -0700

    revert app.run_server

--- src/app.py
+++ src/app.py
@@ -16,5 +16,4 @@ register_callbacks(app)
 
 # Run the app
 if __name__ == "__main__":
-    # app.run_server(debug=False, port=8080)
-    app.run_server(debug=True)
+    app.run_server(debug=False, port=8080)
