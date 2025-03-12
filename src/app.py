4ad70931b7124cea374b9697f2a8a1e3ffeb7ace
Hankun Xiao <xh1319@student.ubc.ca>
2025-03-11 22:45:26 -0700

    solve merge conflict

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 from dash import Dash  # type: ignore
 import dash_bootstrap_components as dbc  # type: ignore
 import dash_vega_components as dvc # type: ignore
-from src.callbacks import register_callbacks  # Import the callback registration function
-from src.components import layout
+from callbacks import register_callbacks  # Import the callback registration function
+from components import layout
 
 # Initialize the Dash app with Bootstrap for styling
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -16,4 +16,5 @@ register_callbacks(app)
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=False, port=8080)
+    # app.run_server(debug=False, port=8080)
+    app.run_server(debug=True)
